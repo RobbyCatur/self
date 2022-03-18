f50891a40d281b03e2ae4acd3115504286e1d15a
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-18 22:42:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,60 @@
+let fs = require('fs')
+let chalk = require('chalk')
+
+global.owner = ['6282245409072'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = ['6285786433832'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'RobbyCatur',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://api.lolhuman.xyz': 'c6670fc7e461b7623a8fdf9f'
+}
+
+// Sticker WM
+
+const spack = fs.readFileSync("lib/exif.json")
+const stickerpack = JSON.parse(spack)
+if (stickerpack.spackname == '') {
+  var sticker_name = 'Pudge Invoker Bot'
+  var sticker_author = 'Robby Catur'
+} else {
+  var sticker_name = stickerpack.spackname
+  var sticker_author = stickerpack.sauthor
+}
+
+const file_exif = "lib/exif.json"
+fs.watchFile(file_exif, () => {
+  fs.unwatchFile(file_exif)
+  console.log(chalk.redBright("Update 'exif.json'"))
+  delete require.cache[file_exif]
+  require('./lib/exif.json')
+})
+
+global.packname = sticker_name
+global.author = sticker_author
+
+
+global.multiplier = 69 // The higher, The harder levelup
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
