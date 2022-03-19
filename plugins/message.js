ecf5cf0fdb10b9d8783e34174bf7e89c2a7244af
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-19 07:51:26 +0700

    Add files via upload

--- plugins/message.js
+++ plugins/message.js
@@ -1,9 +1,14 @@
 let handler = async (m, { conn }) => {
   let user = global.db.data.users[m.sender]
+  if (!m.isGroup) {
   let time = user.time + 86400000
   if (new Date - user.time < 86400000) return
+  }
   let owner = ('6282245409072@s.whatsapp.net')
   if (m.isGroup) return
+  if (!m.isGroup) {
+  let time = user.time + 86400000
+  if (new Date - user.time < 86400000) return
   let teks = `
 Halo, ${user.name}! Saya adalah bot yang dibuat oleh @${owner.replace(/@.+/,'')}. Saya diciptakan untuk menemani Anda selama menunggu @${owner.replace(/@.+/,'')} online kembali
 `.trim()
@@ -24,19 +29,8 @@ Halo, ${user.name}! Saya adalah bot yang dibuat oleh @${owner.replace(/@.+/,'')}
       thumbnail: (await conn.getFile('./src/photo/kay.jpg')).data.toString('base64')
      })
      }
-  handler.customPrefix = /^a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z$/i
+     }
+  handler.customPrefix = /^(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)$/i
   handler.command = new RegExp
   module.exports = handler
-
-  function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
-
-  return hours + " jam " + minutes + " menit"
-}
+  
