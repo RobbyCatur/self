7f739bbc8ebbe99dcf687e62cba80848c7a2f72e
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-19 07:58:43 +0700

    Update message.js

--- plugins/message.js
+++ plugins/message.js
@@ -1,12 +1,7 @@
 let handler = async (m, { conn }) => {
   let user = global.db.data.users[m.sender]
-  if (!m.isGroup) {
-  let time = user.time + 86400000
-  if (new Date - user.time < 86400000) return
-  }
   let owner = ('6282245409072@s.whatsapp.net')
-  if (m.isGroup) return
-  if (!m.isGroup) {
+  if (m.isGroup) throw false
   let time = user.time + 86400000
   if (new Date - user.time < 86400000) return
   let teks = `
@@ -28,7 +23,6 @@ Halo, ${user.name}! Saya adalah bot yang dibuat oleh @${owner.replace(/@.+/,'')}
     }, /*m,*/ { 
       thumbnail: (await conn.getFile('./src/photo/kay.jpg')).data.toString('base64')
      })
-     }
    global.db.data.users[m.sender].time = new Date *1
      }
   handler.customPrefix = /^(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)$/i
